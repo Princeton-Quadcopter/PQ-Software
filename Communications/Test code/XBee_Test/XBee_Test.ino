dbdeb2572ffa1435eff46d53ed6c03700909231c
Byung Cheol Cho <bccho@princeton.edu>
2015-11-20 13:57:20 -0500

    This doesn't work

--- Communications/Test code/XBee_Test/XBee_Test.ino
+++ Communications/Test code/XBee_Test/XBee_Test.ino
@@ -9,39 +9,60 @@ void setup()
   pinMode(3, OUTPUT);
   Serial.begin(9600);
   mySerial.begin(9600);
+  mySerial.listen();
 }
 
 void loop()                     // run over and over again
 {
-  digitalWrite(13, LOW);
+  // digitalWrite(13, HIGH);
+  // while (!mySerial.available()) {
+  //   Serial.print("Stuck...\n");
+  //   if (mySerial.isListening()) {
+  //     Serial.print("but listening\n");
+  //   }
+    
+  //   delay(500);
+  // }
+  // while (mySerial.available()) {
+  //     Serial.print((char)mySerial.read());
+  //     //Serial.print(" ");
+  //     //delay(10);
+  // }
+  // mySerial.flush();
+  // digitalWrite(13, LOW);
+  // delay(1000);
+  Serial.print("About to write\n");
+  sendTransmitRequest(mySerial, 1, 0x0001, 0x00, 5, "hello");
+  //mySerial.flush();
+  while (!mySerial.available()) {
+    Serial.print("Stuck...\n");
+    delay(500);
+  }
   while (mySerial.available()) {
-      digitalWrite(13, HIGH);
       Serial.print((char)mySerial.read());
+      //Serial.print(" ");
       //delay(10);
   }
-  digitalWrite(13, LOW);
-  delay(1000);
-  sendTransmitRequest(mySerial, 1, 0x0000, 0x00, 5, "hello");
-  delay(1000);
+  delay(10000);
 }
 
 // Various methods
 
 void sendTransmitRequest(SoftwareSerial serial, char fID, unsigned int destAddr, char options, char len, char message[])
 {
-//  char checksum = 1 + fID + (destAddr >> 8) + (destAddr % 256) + options;
-//  
-//  serial.print(0x7E);
-//  serial.print(len + 5);
-//  serial.print(1);
-//  serial.print(fID);
-//  serial.print(destAddr);
-//  serial.print(options);
+ char checksum = 1 + fID + (destAddr >> 8) + (destAddr % 256) + options;
+ 
+ serial.print(0x7E);
+ serial.print(len + 5);
+ serial.print(1);
+ serial.print(fID);
+ serial.print(destAddr);
+ serial.print(options);
   for (int i = 0; i < len; i++)
   {
     serial.print(message[i]);
-    //checksum += message[i];
+    checksum += message[i];
   }
   //Serial.print(0xff - checksum, HEX);
-//  serial.print(0xff - checksum);
+  serial.println(0xff - checksum);
 }
--- Communications/Test code/XBee_Test2/XBee_Test2.ino
+++ Communications/Test code/XBee_Test2/XBee_Test2.ino
@@ -1,5 +1,8 @@
 #include "Printers.h"
 #include "XBee.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial = SoftwareSerial(2, 3); // RX, TX
 
 XBee xbee = XBee();
 
@@ -7,15 +10,18 @@ uint8_t payload[] = { 'H', 'i' };
 
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40b3d56e);
 
-Tx16Request tx16 = Tx16Request(addr64, payload, sizeof(payload));
+ZBTxRequest tx16 = ZBTxRequest(addr64, payload, sizeof(payload));
 
 void setup()
 {
+    pinMode(2, INPUT);
+    pinMode(3, OUTPUT);
     pinMode(13, OUTPUT);
     // Start the serial port
     Serial.begin(9600);
     // Tell XBee to use Hardware Serial. It's also possible to use SoftwareSerial
-    xbee.setSerial(Serial);
+    mySerial.begin(9600);
+    xbee.setSerial(mySerial);
 }
 
 void loop()
@@ -25,5 +31,5 @@ void loop()
     delay(500);
     digitalWrite(13, 0);
     delay(500);
-    xbee.receive();
+    //xbee.receive();
 }
